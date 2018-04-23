b4fb9fcfd8ecb8bf6608a9839364fd4a950cadec
Yair Goldshtein <irigo1@gmail.com>
2018-04-23 22:19:20 +0300

    Update lec5.pl

--- Lectures/lec5.pl
+++ Lectures/lec5.pl
@@ -9,3 +9,22 @@ c(4).
 d(1,4).
 d(2,3).
 e(3).
+
+%count([H|T],RES):-count(T,RES1),RES is RES1+1.
+%count([],0).
+
+count(List,RES):-count(List,0,RES).
+count([],RES,RES).
+count([_|T],ACC,RES):-ACC1 is ACC+1,
+    count(T,ACC1,RES).
+
+%reverse([],[]).
+%reverse([H|T],Rev):-reverse(T,R),
+  %  append(R,[H],Rev).
+
+reverse(List,Rev):-reverse(List,[],Rev).
+reverse([],Rev,Rev).
+reverse([H|T],ACC,Rev):-
+    reverse(T,[H|ACC],Rev).
+
+
